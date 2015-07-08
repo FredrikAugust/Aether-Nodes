364963e1b1dd2c84ca65b983bd5362bd2ca8ba58
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-08 16:01:23 +0200

    increased timeout javascript to 5 minutes

--- src/static/site.js
+++ src/static/site.js
@@ -8,7 +8,7 @@ function getOnline() {
 		$.ajax({
 			type: 'POST',
 			url: $(element).data('url'),
-			timeout: 10000,
+			timeout: 300000,
 			error: function(x, t, m) {
 		        if(t === "timeout") {
 					var onlineChild = $(element).find('.online');
